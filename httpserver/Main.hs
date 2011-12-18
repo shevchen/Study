dbd96e880a10782b54cf52f0a9ff50d1706e3b0f
Dmitry Shevchenko <shevchen239@gmail.com>
2011-12-18 19:23:50 +0400

    :q

--- httpserver/Main.hs
+++ httpserver/Main.hs
@@ -3,10 +3,16 @@ module Main where
 import System.Environment (getArgs)
 import Network.Socket
 import Data.Word (Word16)
+import Control.Concurrent (forkIO)
 
 main = do
   args <- getArgs
   let port = read (head args)::Word16
   mySocket <- socket AF_INET Stream defaultProtocol
   bindSocket mySocket $ SockAddrInet (PortNum port) 0
+  listen mySocket 10
+  receiveMessages mySocket
   putStrLn $ head args
+
+receiveMessages mySocket = do
+  forkIO $ recv mySocket 1024 >>= putStrLn
