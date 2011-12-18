8701dca6612c330ff1aa668f2781ad37aae4abbd
Dmitry Shevchenko <shevchen239@gmail.com>
2011-12-18 23:38:13 +0400

    Refactoring.

--- httpserver/Main.hs
+++ httpserver/Main.hs
@@ -22,34 +22,29 @@ receiveMessages mySocket = do
 
 respond :: Handle -> IO ()
 respond socketHandle = do
-  firstLine <- hGetLine socketHandle
-  case (getData $ words firstLine) of
-    Just ("GET", address, "HTTP/1.1") -> respondToMsg socketHandle address
-    _                                 -> return ()
-  respond socketHandle
+  isClosed <- hIsEOF socketHandle
+  if isClosed then return () else do
+    firstLine <- hGetLine socketHandle
+    case (getData $ words firstLine) of
+      Just ("GET", address, "HTTP/1.1") -> respondToMsg socketHandle address
+      _                                 -> return ()
+    respond socketHandle
 
 getData :: [String] -> Maybe (String, String, String)
-getData []     = Nothing
-getData (x:xs) = getRest x xs []
-
-getRest :: String -> [String] -> String -> Maybe (String, String, String)
-getRest method [] _            = Nothing
-getRest method (x:[]) addr     = Just (method, addr, x)
-getRest method (x:(y:ys)) addr = getRest method (y:ys) (addr ++ x)
+getData (x:(y:(z:[]))) = Just (x, y, z)
+getData _              = Nothing
 
 respondToMsg :: Handle -> String -> IO ()
 respondToMsg socketHandle address = do
   exists <- doesFileExist address
-  if not exists then hPutStrLn socketHandle "HTTP/1.1 404 Not Found" else 
-    hPutStrLn socketHandle "HTTP/1.1 200 OK" >>
-    hPutStrLn socketHandle "" >>
-    openFile address ReadMode >>=
-    writeToFile socketHandle
+  if not exists then hPutStrLn socketHandle "HTTP/1.1 404 Not Found" else do
+    hPutStrLn socketHandle "HTTP/1.1 200 OK"
+    hPutStrLn socketHandle ""
+    openFile address ReadMode >>= writeToFile socketHandle
 
 writeToFile :: Handle -> Handle -> IO ()
 writeToFile socketHandle file = do
   isEnd <- hIsEOF file
-  if isEnd then hClose file else
-    hGetLine file >>=
-    hPutStrLn socketHandle >>
+  if isEnd then hClose file else do
+    hGetLine file >>= hPutStrLn socketHandle
     writeToFile socketHandle file
