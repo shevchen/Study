9845d1707a3dc5aca0ded1471ca239c9f63a9b29
Dmitry Shevchenko <shevchen239@gmail.com>
2011-12-24 00:28:16 +0400

    I am so fucking lazy.

--- parsenormalizer/Main.hs
+++ parsenormalizer/Main.hs
@@ -4,13 +4,14 @@ import Text.Parsec.String
 import Text.Parsec.Token
 import Text.Parsec.Language
 import System.Environment
+import System.IO
 import Lambda hiding (main)
 
 def = emptyDef{ identStart = letter
               , identLetter = alphaNum
-              , opStart = oneOf ".\\"
-              , opLetter = oneOf ".\\"
-              , reservedOpNames = ["\\"]
+              , opStart = oneOf ".\\-"
+              , opLetter = oneOf ".\\->"
+              , reservedOpNames = ["\\", ".", "->"]
               }
 
 TokenParser{ parens = m_parens
@@ -19,30 +20,32 @@ TokenParser{ parens = m_parens
            , whiteSpace = m_whiteSpace } = makeTokenParser def
 
 parser :: Parser Term
-parser = m_whiteSpace >> termparser <* eof
-    where
-      termparser :: Parser Term
-      termparser = do
-          { m_reservedOp "\\"
-          ; var <- m_identifier
-          ; m_reservedOp "."
-          ; tm <- termparser
-          ; return (Abs var tm)
-          }
-          <|> do
-          { a <- termparser
-          ; b <- termparser
-          ; return (App a b)
-          }
-          <|> do
-          { a <- termparser
-          ; return a
-          }
+parser = m_whiteSpace >> termparser <* eof where
+    termparser :: Parser Term
+    termparser = do
+        { inside <- m_parens termparser
+        ; option inside (do {other <- termparser; return (App inside other)})
+        }
+        <|> do
+        { m_reservedOp "\\"
+        ; var <- m_identifier
+        ; (m_reservedOp ".") <|> (m_reservedOp "->")
+        ; tm <- termparser
+        ; return (Abs var tm)
+        }
+        {-<|> do
+        { a <- termparser
+        ; b <- termparser
+        ; return (App a b)
+        }-}
+        <|> do
+        { a <- m_identifier
+        ; return (Var a)
+        }
 
 main :: IO ()
 main = do
     args <- getArgs
     case parse parser "" (head args) of
-             { Left err -> print err
-             ; Right ans -> print ans
-             }
+        Left err -> print err
+        Right ans -> print ans
