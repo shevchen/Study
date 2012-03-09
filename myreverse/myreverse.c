85290c5c7b9f56ba25b8e33d66c643d35af95090
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-09 17:09:34 +0400

    myreverse, second version

--- myreverse/myreverse.c
+++ myreverse/myreverse.c
@@ -2,110 +2,104 @@
 
 #define buf_size 10
 
-struct buffer
+typedef struct buffer
 {
-  char buf[buf_size], temp_buf[buf_size];
-  int busy_pos, free_pos;
-};
+  char in[buf_size], out[buf_size];
+  int in_size, out_size, skip_next;
+} buf_t;
 
-int read_chars(struct buffer* buf)
+int read_chars(buf_t* buf)
 {
-  int first = read(0, buf->buf + buf->free_pos, buf_size - buf->free_pos);
-  if (first == -1)
-  {
-    return -1;
+  int count = read(0, buf->in, buf_size);
+  if (count >= 0)
+  { 
+    buf->in_size = count;
   }
-  if (buf->free_pos + first < buf_size)
-  {
-    buf->free_pos += first;
-    return first;
-  }
-  int second = read(0, buf->buf, buf->busy_pos); 
-  if (second == -1)
-  {
-    return -1;
-  }
-  buf->free_pos = second;
-  return first + second;
+  return count;
 }
 
-int reverse(struct buffer* buf, int from, int to)
+int reverse(char* out, int len)
 {
-  int len = 0;
-  void* a;
-  if (to >= from)
-  {
-    len = to - from + 1;
-    a = memcpy(buf->temp_buf, buf->buf + from, len);
-  }
-  else
-  {
-    len = buf_size - from;
-    a = memcpy(buf->temp_buf, buf->buf + from, len);
-    len += to + 1;
-    a = memcpy(buf->temp_buf + buf_size - from, buf->buf, to + 1);
-  }
   int i;
   for (i = 0; i < (len - 1) / 2; ++i)
   {
-    char c = buf->temp_buf[i];
-    buf->temp_buf[i] = buf->temp_buf[len - 2 - i];
-    buf->temp_buf[len - 2 - i] = c;
+    char c = out[i];
+    out[i] = out[len - 2 - i];
+    out[len - 2 - i] = c;
   }
-  return write(1, buf->temp_buf, len);
+  return write(1, out, len);
 }
 
-int next_pos(int pos)
+int write_chars(buf_t* buf)
 {
-  return (pos + 1) % buf_size;
-}
-
-int write_chars(struct buffer* buf)
-{
-  int written = 0;
-  while (1)
+  int pos = 0;
+  while (pos < buf->in_size)
   {
-    int i;
-    for (i = buf->busy_pos; i != buf->free_pos; i = next_pos(i))
+    int start = pos;
+    while (pos < buf->in_size && buf->in[pos] != '\n')
     {
-      if (buf->buf[i] == '\n')
+      pos++;
+    }
+    if (pos == buf->in_size)
+    {
+      if (start == 0)
       {
-        break;
+        buf->skip_next = 1;
+        buf->out_size = 0;
+      }
+      else
+      {
+        memcpy(buf->out, buf->in + start, pos - start);
+        buf->out_size = pos - start;
       }
     }
-    if (i == buf->free_pos)
+    else
     {
-      buf->busy_pos = buf->free_pos;
-      return written;
-    }
-    int reversed = reverse(buf, buf->busy_pos, i);
-    if (reversed == -1)
-    {
-      return -1;
-    }
-    written += reversed;
-    buf->busy_pos = next_pos(i);
+      if (buf->skip_next || buf->out_size + pos - start + 1 > buf_size)
+      {
+        buf->skip_next = 0;
+      }
+      else
+      {
+        memcpy(buf->out + buf->out_size, buf->in + start, pos - start + 1);
+        if (reverse(buf->out, buf->out_size + pos - start + 1) == -1)
+        {
+          return -1;
+        }
+      }
+      buf->out_size = 0;
+      pos++;
+    }    
   }
-  return written;
+  return 0;
 }
 
 int main()
 {
-  struct buffer buf;
-  buf.busy_pos = 0;
-  buf.free_pos = 0;
+  buf_t* buf;
+  buf->in_size = 0;
+  buf->out_size = 0;
+  buf->skip_next = 0;
   int count;
   do
   {
-    count = read_chars(&buf);
+    count = read_chars(buf);
     if (count == -1)
     {
       return -1;
     }
-    if (write_chars(&buf) == -1)
+    if (write_chars(buf) == -1)
     {
       return -1;
     }
   } while (count == buf_size);
+  if (buf->out_size > 0)
+  {
+    buf->out[buf->out_size++] = '\n';
+    if (write_chars(buf) == -1)
+    {
+      return -1;
+    }
+  }
   return 0;
 }
