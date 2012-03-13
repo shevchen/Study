6afe6a55076079ca6bf5a89c4234fcf1416fa5e2
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-13 20:20:41 +0400

    EOF detection

--- myreverse/myreverse.c
+++ myreverse/myreverse.c
@@ -4,75 +4,98 @@
 
 typedef struct
 {
-  char in[buf_size], out[buf_size];
-  int in_size, out_size, skip_next;
+  char in[buf_size];
+  int in_size, skip_next;
 } buf_t;
 
 int read_chars(buf_t* buf)
 {
-  int count = read(0, buf->in, buf_size);
-  if (count >= 0)
-  { 
-    buf->in_size = count;
+  int was = buf->in_size;
+  while (buf->in_size < buf_size)
+  {
+    int count = read(0, buf->in + buf->in_size, buf_size - buf->in_size);
+    if (count <= 0)
+    {
+      return count;
+    }
+    buf->in_size += count;
   }
-  return count;
+  return buf->in_size - was;
+}
+
+int write_chars(char* buf, int len)
+{
+  int pos = 0;
+  while (pos < len)
+  {
+    int count = write(1, buf + pos, len - pos);
+    if (count == -1)
+    {
+      return -1;
+    }
+    pos += count;
+  }
+  return 0;
 }
 
 int reverse(char* out, int len)
 {
-  int i;
-  int act_len = len;
-  while (act_len > 0 && (out[act_len - 1] == '\n' || out[act_len - 1] == '\r'))
+  int left = 0, right = len - 1;
+  while (right >= 0 && (out[right] == '\n' || out[right] == '\r'))
   {
-    act_len--;
+    right--;
   }
-  for (i = 0; i < act_len / 2; ++i)
+  while (left < right) 
   {
-    char c = out[i];
-    out[i] = out[act_len - 1 - i];
-    out[act_len - 1 - i] = c;
+    char c = out[left];
+    out[left] = out[right];
+    out[right] = c;
+    left++, right--;
   }
-  return write(1, out, len);
+  return write_chars(out, len);
 }
 
-int write_chars(buf_t* buf)
+int process(buf_t* buf, int last)
 {
   int pos = 0;
-  while (pos < buf->in_size)
+  int sz = buf->in_size;
+  buf->in_size = 0;
+  while (pos < sz)
   {
     int start = pos;
-    while (pos < buf->in_size && buf->in[pos] != '\n')
+    while (pos < sz && buf->in[pos] != '\n')
     {
       pos++;
     }
-    if (pos == buf->in_size)
+    if (pos == sz && last)
+    {
+      pos--;
+    }
+    if (pos == sz)
     {
       if (start == 0)
       {
         buf->skip_next = 1;
-        buf->out_size = 0;
       }
       else
       {
-        memcpy(buf->out, buf->in + start, pos - start);
-        buf->out_size = pos - start;
+        memmove(buf->in, buf->in + start, pos - start);
+        buf->in_size = pos - start;
       }
     }
     else
     {
-      if (buf->skip_next || buf->out_size + pos - start + 1 > buf_size)
+      if (buf->skip_next)
       {
         buf->skip_next = 0;
       }
       else
       {
-        memcpy(buf->out + buf->out_size, buf->in + start, pos - start + 1);
-        if (reverse(buf->out, buf->out_size + pos - start + 1) == -1)
+        if (reverse(buf->in + start, pos - start + 1) == -1)
         {
           return -1;
         }
       }
-      buf->out_size = 0;
       pos++;
     }    
   }
@@ -83,26 +106,21 @@ int main()
 {
   buf_t buf;
   buf.in_size = 0;
-  buf.out_size = 0;
   buf.skip_next = 0;
-  int count;
-  do
+  while (1)
   {
-    count = read_chars(&buf);
+    int count = read_chars(&buf);
     if (count == -1)
     {
       return -1;
     }
-    if (write_chars(&buf) == -1)
+    if (process(&buf, count == 0) == -1)
     {
       return -1;
     }
-  } while (count == buf_size);
-  if (buf.out_size > 0)
-  {
-    if (reverse(buf.out, buf.out_size) == -1)
+    if (count == 0)
     {
-      return -1;
+      break;
     }
   }
   return 0;
