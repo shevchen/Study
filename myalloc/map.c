7ec82de8104aeb609fd2a26d4e40cfcca663f871
Dmitry Shevchenko <shevchen239@gmail.com>
2012-04-06 23:42:42 +0400

    some troubles with free

--- myalloc/map.c
+++ myalloc/map.c
@@ -2,23 +2,28 @@
 
 static bucket_list* map[MOD];
 
-static void* get_buckets(pid_t pid, int small) {
+static bucket_list* get_all_buckets(pid_t pid) {
   size_t hash = get_hash(pid);
   bucket_list* list = map[hash];
   while (list != NULL) {
     if (list->pid == pid) {
-      return small ? (void*)list->small : (void*)list->large;
+      return list;
     }
     list = list->next;
   }
   bucket_list new_list = {
     .pid = pid,
-    .small =  NULL,
-    .large =  NULL,
+    .small = NULL,
+    .large = NULL,
     .next = map[hash]
   };
   map[hash] = &new_list;
-  return small ? (void*)(new_list.small) : (void*)(new_list.large);
+  return map[hash];
+}
+
+static void* get_buckets(pid_t pid, int small) {
+  bucket_list* list = get_all_buckets(pid);
+  return small ? (void*)(list->small) : (void*)(list->large);
 }
 
 small_bucket* get_small_buckets(pid_t pid) {
@@ -36,7 +41,7 @@ void add_small_bucket(pid_t pid, small_bucket* new_bucket) {
 }
 
 void add_large_bucket(pid_t pid, large_bucket* new_bucket) {
-  bucket_list* list = map[get_hash(pid)];
+  bucket_list* list = get_all_buckets(pid);
   new_bucket->next = list->large;
   list->large = new_bucket;
 }
