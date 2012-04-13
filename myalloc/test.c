8046c93e95c5c3a721e94f636e70d32597579790
Dmitry Shevchenko <shevchen239@gmail.com>
2012-04-13 14:37:59 +0400

    fail test found

--- myalloc/test.c
+++ myalloc/test.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
+#include <unistd.h>
 
 void main() {
   int* mem = (int*)malloc(200);
-  printf("allocated mem\n");
   int i;
   for (i = 20; i < 80; ++i) {
     mem[i] = i;
   }
+  mem[0] = 1;
+  mem[199] = 1;
   int sum = 0;
   for (i = 30; i <= 50; ++i) {
     sum += mem[i];
   }
   printf("%d = 840\n", sum);
   free(mem);
-  printf("freed mem\n");
   mem = (int*)malloc(305);
-  printf("allocated mem\n");
+  mem[0] = 239;
+  mem[304] = 366;
   size_t* mem2 = (size_t*)malloc(1000000);
-  printf("allocated mem2\n");
   mem2[123456] = 654321;
+  mem[0] = 0;
+  mem[999999] = 999999;
   printf("%d = 654321\n", mem2[123456]);
   free(mem2);
-  printf("freed mem2\n");
   free(mem);
-  printf("freed mem\n");
 }
