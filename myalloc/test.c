6a770e065447a1a68b24f2d3a4dfd4188b864a91
Dmitry Shevchenko <shevchen239@gmail.com>
2012-04-13 21:41:15 +0400

    new test

--- myalloc/test.c
+++ myalloc/test.c
@@ -1,27 +1,13 @@
 #include <unistd.h>
 
 void main() {
-  int* mem = (int*)malloc(200 * sizeof(int));
+  int ITER = 1000;
+  int* mem[ITER];
   int i;
-  for (i = 20; i < 80; ++i) {
-    mem[i] = i;
+  for (i = 0; i < ITER; ++i) {
+    mem[i] = (int*)malloc(sizeof(int) * (i % 23 + i % 2 * 1000));
   }
-  mem[0] = 1;
-  mem[199] = 1;
-  int sum = 0;
-  for (i = 30; i <= 50; ++i) {
-    sum += mem[i];
+  for (i = 0; i < ITER; ++i) {
+    free(mem[i]);
   }
-  printf("%d = 840\n", sum);
-  free(mem);
-  mem = (int*)malloc(30 * sizeof(int));
-  mem[0] = 239;
-  mem[29] = 366;
-  size_t* mem2 = (size_t*)malloc(1000000 * sizeof(size_t));
-  mem2[123456] = 654321;
-  mem2[0] = 0;
-  mem2[999999] = 999999;
-  printf("%d = 654321\n", mem2[123456]);
-  free(mem2);
-  free(mem);
 }
